eb7c4e6965054ace1ab48e611ba052f14d282d55
anton <antonche32@gmail.com>
2024-05-09 16:08:26 +0300

    add code files

---
+++ dmodel.py
@@ -0,0 +1,145 @@
+import argparse
+import datetime
+import json
+
+
+# Класс исключения
+class EInvDateFormat(Exception):
+    pass
+
+# Распечатать запись
+def print_rec(r):
+    """ Распечатать запись """
+    print("%d\t%s\t%s\t%d\t%s"%(r["id"], r["date"].strftime("%d.%m.%y/%H:%M"),
+                                "Расход" if r["category"] == "outgoing" else "Приход",
+                                   r["summ"],r["desc"]))
+
+
+class DB:
+    """
+    Класс для работы с данными и конфигурацией
+    """
+    def __init__(self):
+        """
+        по ключу conf - хранится конфгурция
+        по ключу data - хранятся данные
+        last_id - последний идентификатор записи
+        """
+        self.db = {}
+        self.db["conf"] = {}
+        self.db["data"] = []
+        self.last_id = -1
+
+    def load(self, name:str):
+        """
+        @name - имя файла
+        Загружает конфигурацию и данные из файла в формате json
+        """
+        try:
+            self.db = json.load(open(name, "rt"))
+            _id = 0
+            for d in self.db["data"]:
+                # Конвертировать все строки с датой в обьекты datetime
+                d["date"] = datetime.datetime.fromisoformat(d["date"])
+                if d["id"] > _id:
+                    _id = d["id"]
+            self.last_id = _id
+        except FileNotFoundError:
+            pass
+
+    def save(self, name:str):
+        """
+        @name - имя файла
+        Сохранить в фаил
+        """
+        for d in self.db["data"]:
+            # Конветировать все обьекты datetime в строки
+            d["date"] = d["date"].isoformat()
+        json.dump(self.db, open(name, "wt"))
+
+    def append_incoming(self, summ:int, desc:str=""):
+        """
+        @summ - сумма
+        @desc - описание
+        Добавить запись прихода
+        """
+        self.last_id += 1
+        # Добавить запись прихода
+        self.data_add = {"id":self.last_id,"date":datetime.datetime.now(), "summ":summ, "category":"incoming","desc":desc}
+
+    def append_outgoing(self, summ:int, desc:str=""):
+        """
+        @summ - сумма
+        @desc - описание
+        Добавить запись расхода
+        """
+        self.last_id += 1
+        self.data_add = {"id":self.last_id,"date":datetime.datetime.now(), "summ":summ, "category":"outgoing","desc":desc}
+
+    def edit(self, id, e):
+        r = self.get_by_id(id)
+        for i in e:
+            r[i] = e[i]
+
+
+    def get_by_id(self, id) -> {}:
+        """
+        @id - идентфикатор записи
+        Получить запись по id
+        """
+        for r in self.data_get:
+            if r["id"] == id:
+                return r
+
+    def find(self,  args: argparse.Namespace) -> []:
+        """
+        @agrs - здесь условия поиска
+         Поиск записей
+        """
+        dd = ()
+        if args.fbd is not None:
+            dd = args.fbd.split("-")
+        if len(dd) == 1:
+            # Если нет разделителя "-" значит указана одна дата
+            bd = datetime.datetime.strptime(dd[0], "%d.%m.%y")
+            # Диапазон + сутки
+            ed = datetime.datetime.fromtimestamp(bd.timestamp()+24*60*60)
+        elif len(dd) == 2:
+            # Разделитель есть значит даты 2е
+            bd = datetime.datetime.strptime(dd[0], "%d.%m.%y")
+            ed = datetime.datetime.strptime(dd[1], "%d.%m.%y")
+        elif len(dd) == 0:
+            pass
+        else:
+            raise EInvDateFormat()
+        res = []
+        for r in self.data_get:
+            if (args.fbd is None or ( r["date"] >= bd and r["date"] < ed )) and \
+                    (args.fbs is None or ( r["summ"] == args.fbs )) and \
+                    (args.fbc is None or (args.fbc in r["desc"])):
+                res.append(r)
+        return res
+
+    def remove(self, id:int):
+        """
+        @id - Идентификатор удаляемой записи
+        Удалить запись с id
+        """
+        self.db["data"].remove(self.get_by_id(id))
+
+    def __getattr__(self, item):
+        if item.startswith("conf_"):
+            return self.db["conf"][item[5:]]
+        elif item == "data_get":
+            return self.db["data"]
+        # чтобы не было рекурсии
+        return self.__getattribute__(item)
+
+    def __setattr__(self, key, value):
+        if key.startswith("conf_"):
+            self.db["conf"][key[5:]]=value
+        if key == "data_add":
+            self.db["data"].append(value)
+        else:
+            # чтобы не было рекурсии
+            object.__setattr__(self, key, value)
---
+++ wallet.py
@@ -0,0 +1,92 @@
+import argparse
+import datetime
+from operator import attrgetter
+import dmodel
+import locale
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Микро бухгалтерия, для кошелька")
+    # subparse = parser.add_subparsers(title="action", dest="action")
+    group = parser.add_mutually_exclusive_group()
+    # Приход - опция задающая количество поступивших денег
+    group.add_argument("-i", "--incoming", dest="incoming", help="Приход", type=int)
+    # Расход - опция задающая количество израсходованных денег
+    group.add_argument("-o", "--outgoing", dest="outgoing", help="Расход", type=int)
+    # Задает фильтрацию по дате
+    parser.add_argument("-d", "--find-by-date", dest="fbd", help="Поиск по дате 5.5.24 - только за это день или 5.5.24-6.5.24 тоже самое только диапазон", type=str)
+    # Задает фильтрацию по сумме
+    parser.add_argument("-s", "--find-by-summ", dest="fbs", help="Поиск по сумме", type=int)
+    # Задает фильтрацию по включению в описание
+    parser.add_argument("-c", "--find-by-desc", dest="fbc", help="Поиск по включению в описание", type=str)
+    # Описание
+    parser.add_argument("-x", "--desc", dest="desc", help="Описание", type=str)
+    # Определяет дату и время в режиме редактирования
+    parser.add_argument("-t", "--datetime", dest="datetime", help="Дата/Время - 8.5.24/10:24",type=str)
+    # Режим редактирования -i и -o пределяют категорию и сумму, -x описание, -t дату/время
+    parser.add_argument("-e", "--edit", dest="edit", help="Редактирование записи с id, -i и -o пределяют категорию и сумму, -x описание, -t дату/время", type=int)
+    parser.add_argument("-r", "--remove", dest="remove", help="Удаление записи с id", type=int)
+    # Один или более файлов для обработки
+    parser.add_argument("files", metavar="data_files", type=str, nargs="+", help="Файлы данных")
+    args = parser.parse_args()
+    # если описание не определено то равно пустой строке
+    if args.desc is None:
+        args.desc = ""
+    # для всех файлов
+    for f in args.files:
+        # Создать объект хранилища
+        db = dmodel.DB()
+        # Загрузить данные из файла
+        db.load(f)
+        # Если режим редактирования
+        if args.edit is not None:
+            # Создать пустую запись
+            r = {}
+            # Если установлен ключ  -i
+            if args.incoming is not None:
+                r["category"] = "incoming"
+                r["summ"] = args.incoming
+            # Если установлен ключ -o
+            if args.outgoing is not None:
+                r["category"] = "outgoing"
+                r["summ"] = args.outgoing
+            # Если установлен ключ -t
+            if args.datetime is not None:
+                r["date"] = datetime.datetime.strptime(args.datetime, "%d.%m.%y/%H:%M")
+            # Если установлен ключ -x
+            if args.desc is not None:
+                r["desc"] = args.desc
+            # Редактировать запись
+            db.edit(args.edit, r)
+        # Режим добавления записи
+        elif args.remove is not None:
+            db.remove(args.remove)
+        else:
+            if args.incoming is not None:
+                # Добавить приход
+                db.append_incoming(args.incoming, desc=args.desc)
+            if args.outgoing is not None:
+                # Добавить расход
+                db.append_outgoing(args.outgoing, desc=args.desc)
+            # Всего прищло
+            i = 0
+            # Всего ушло
+            o = 0
+            # Фильтруем по условиям поиска
+            for r in db.find(args):
+                dmodel.print_rec(r)
+                if r["category"] == "incoming":
+                    # Считаем общий приход
+                    i += r["summ"]
+                else:
+                    # Считаем общий расход
+                    o += r["summ"]
+            # Напечатать приход, расход, баланс
+            print("Приход:%d - Расход:%d = Баланс:%d"%(i, o, i-o))
+        # Сохранить данные
+        db.save(f)
+
+
+if __name__ == "__main__":
+    main()
